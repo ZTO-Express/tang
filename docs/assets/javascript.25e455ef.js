b2d224f9e4e5c002f57c466614c80f0a1487380c
rayl <liuyi86@zto.com>
2023-01-10 14:39:13 +0800

    feat: 构建github docs文档

---
+++ docs/assets/javascript.25e455ef.js
@@ -0,0 +1,73 @@
+import { conf as conf$1, language as language$1 } from "./typescript.778b8e75.js";
+import "./main.cd448869.js";
+/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.33.0(4b1abad427e58dbedc1215d99a0902ffc885fcd4)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/
+var conf = conf$1;
+var language = {
+  defaultToken: "invalid",
+  tokenPostfix: ".js",
+  keywords: [
+    "break",
+    "case",
+    "catch",
+    "class",
+    "continue",
+    "const",
+    "constructor",
+    "debugger",
+    "default",
+    "delete",
+    "do",
+    "else",
+    "export",
+    "extends",
+    "false",
+    "finally",
+    "for",
+    "from",
+    "function",
+    "get",
+    "if",
+    "import",
+    "in",
+    "instanceof",
+    "let",
+    "new",
+    "null",
+    "return",
+    "set",
+    "super",
+    "switch",
+    "symbol",
+    "this",
+    "throw",
+    "true",
+    "try",
+    "typeof",
+    "undefined",
+    "var",
+    "void",
+    "while",
+    "with",
+    "yield",
+    "async",
+    "await",
+    "of"
+  ],
+  typeKeywords: [],
+  operators: language$1.operators,
+  symbols: language$1.symbols,
+  escapes: language$1.escapes,
+  digits: language$1.digits,
+  octaldigits: language$1.octaldigits,
+  binarydigits: language$1.binarydigits,
+  hexdigits: language$1.hexdigits,
+  regexpctl: language$1.regexpctl,
+  regexpesc: language$1.regexpesc,
+  tokenizer: language$1.tokenizer
+};
+export { conf, language };
---
+++ docs/assets/site.ae0fc49d.js
@@ -0,0 +1,1 @@
+import "./main.cd448869.js";
